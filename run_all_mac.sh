5e41b255538b864ff56af2ad55910c46f3309fa0
Adeel Ahmed <adeel718@googlemail.com>
2020-01-22 17:07:38 +0000

    NO-JIRA Updated mac script to run all services in new tabs instead

--- run_all_mac.sh
+++ run_all_mac.sh
@@ -2,19 +2,20 @@
 export hse=$(pwd)
 #newtab='tell application "System Events" to keystroke "t" using command down'
 echo $hse
+#setjdk 11
 
 export NOTIFY_API_KEY="${NOTIFY_API_KEY:-test-key}"
 export GOOGLE_ANALYTICS_TRACKING_ID="test-key"
 echo 'Notify key being used is: ' $NOTIFY_API_KEY
 
-#osascript -e 'tell application "Terminal"' -e 'activate' -e 'to do script cd $hse/htbhf-applicant-web-ui; git status; npm start; pause.sh' -e 'end tell'
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-applicant-web-ui; git status; npm start; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-applicant-web-ui; git status; npm run test:session-details; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-claimant-service; git status; sleep 5; ./gradlew bootRun; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-eligibility-service; git status; ./gradlew bootRun; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-dwp-api; git status; ./gradlew bootRun; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-hmrc-api; git status; ./gradlew bootRun; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-card-services-api; git status; ./gradlew bootRun; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-smart-stub; git status; ./gradlew bootRun; pause.sh\""
-osascript -e "tell application \"Terminal\" to do script \"cd $hse/htbhf-os-places-stub; git status; ./gradlew bootRun; pause.sh\""
+hash ttab 2>/dev/null || { brew install https://raw.githubusercontent.com/mklement0/ttab/master/ttab.rb; exit 1; }
 
+ttab -t 'applicant-web-ui' -s Grass 'cd htbhf-applicant-web-ui; git status; npm start;'
+ttab -t 'session applicant-web-ui' -s Grass 'cd htbhf-applicant-web-ui; git status; npm run test:session-details;'
+ttab -t 'claimant-service' -s Grass 'cd htbhf-claimant-service; git status; sleep 5; ./gradlew bootRun;'
+ttab -t 'eligibility-service' -s Grass 'cd htbhf-eligibility-service; git status; ./gradlew bootRun;'
+ttab -t 'dwp-api' -s Grass 'cd htbhf-dwp-api; git status; ./gradlew bootRun;'
+ttab -t 'hmrc-api' -s Grass 'cd htbhf-hmrc-api; git status; ./gradlew bootRun;'
+ttab -t 'card-services-api' -s Grass 'cd htbhf-card-services-api; git status; ./gradlew bootRun;'
+ttab -t 'smart-stub' -s Grass 'cd htbhf-smart-stub; git status; ./gradlew bootRun;'
+ttab -t 'os-places-stub' -s Grass 'cd htbhf-os-places-stub; git status; ./gradlew bootRun;'
